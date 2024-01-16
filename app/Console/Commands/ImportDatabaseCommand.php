0cd7973c4d5c6769a66afde12ee831d3c997bf7f
DavieK2 <daviekingz.dk@gmail.com>
2024-01-16 08:56:20 +0100

    New Updates

--- app/Console/Commands/ImportDatabaseCommand.php
+++ app/Console/Commands/ImportDatabaseCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Modules\SchoolManager\Models\StudentProfileModel;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -15,6 +16,14 @@ class ImportDatabaseCommand extends Command
         set_time_limit(0);
         ini_set('memory_limit', '-1');
         
-        DB::unprepared(file_get_contents(base_path('cbt.sql')));
+        $students = DB::table('student_profiles_update')->where('student_code', 'like', '%SOBMCAL/21%')->where('class_id', 3)->get();
+
+        $students->each(function( $student ){
+
+            StudentProfileModel::firstWhere('student_code', $student->student_code)?->update([ 'profile_pic' => $student->profile_pic ]);
+
+        });
+
+        // DB::unprepared(file_get_contents(base_path('cbt.sql')));
     }
 }
