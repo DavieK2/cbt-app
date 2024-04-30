1188002366348f493db9c3c1f22006d00c944dda
DavieK2 <daviekingz.dk@gmail.com>
2024-04-30 20:16:20 +0100

    Updates

--- app/Modules/CBT/Controllers/AssessmentResultController.php
+++ app/Modules/CBT/Controllers/AssessmentResultController.php
@@ -94,7 +94,7 @@ class AssessmentResultController extends Controller
                     ( $total_score >= 50 && $total_score <= 59 ) => 'C',
                     ( $total_score >= 45 && $total_score <= 49 ) => 'D',
                     ( $total_score >= 40 && $total_score <= 44 ) => 'D',
-                    ( $total_score < 39 ) => 'F',
+                    ( $total_score <= 39 ) => 'F',
                     default => NULL
                 };
 
