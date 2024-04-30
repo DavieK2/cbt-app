370bea22978f9e2111d3460b61f2bb5cdd56dd8f
DavieK2 <daviekingz.dk@gmail.com>
2024-04-30 20:22:31 +0100

    Updates

--- app/Modules/CBT/Controllers/AssessmentResultController.php
+++ app/Modules/CBT/Controllers/AssessmentResultController.php
@@ -90,10 +90,10 @@ class AssessmentResultController extends Controller
                 
                 $grade = match( true ){
                     ( $total_score >= 70 ) => 'A',
-                    ( $total_score >= 60 && $total_score <= 69 ) => 'B',
-                    ( $total_score >= 50 && $total_score <= 59 ) => 'C',
+                    ( $total_score > 59 && $total_score < 70 ) => 'B',
+                    ( $total_score > 49 && $total_score <= 59 ) => 'C',
                     ( $total_score >= 45 && $total_score <= 49 ) => 'D',
-                    ( $total_score >= 40 && $total_score <= 44 ) => 'E',
+                    ( $total_score > 39 && $total_score <= 44 ) => 'E',
                     ( $total_score <= 39 ) => 'F',
                     default => NULL
                 };
