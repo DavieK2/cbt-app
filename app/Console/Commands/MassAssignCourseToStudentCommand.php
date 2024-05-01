a1b69b10be4be67e9ed5074fe2526f7016139088
DavieK2 <daviekingz.dk@gmail.com>
2024-05-01 09:15:16 +0100

    Updates

--- app/Console/Commands/MassAssignCourseToStudentCommand.php
+++ app/Console/Commands/MassAssignCourseToStudentCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Modules\SchoolManager\Models\ClassModel;
 use App\Modules\SchoolManager\Models\StudentProfileModel;
 use App\Modules\SchoolManager\Models\SubjectModel;
 use Illuminate\Console\Command;
@@ -14,11 +15,19 @@ class MassAssignCourseToStudentCommand extends Command
 
     public function handle()
     {
-       $courses = SubjectModel::latest()->limit(7)->get()->pluck('uuid')->toArray();
+        StudentProfileModel::where('student_code', 'like', '%SOBNCAL/22/%')->get()->each(function($student) {
 
-        StudentProfileModel::where('student_code', 'like', '%SOBMCAL/22/%')->get()->each(function($student) use($courses){
+            $class = ClassModel::firstWhere('class_name', '200 LEVEL')->uuid;
 
-            $student->assignSubject( $courses );
+            $student->update(['class_id' =>  $class ]);
+
+        });
+
+        StudentProfileModel::where('student_code', 'like', '%SOBNCAL/21/%')->get()->each(function($student) {
+
+            $class = ClassModel::firstWhere('class_name', '300 LEVEL')->uuid;
+
+            $student->update(['class_id' =>  $class ]);
 
         });
     }
